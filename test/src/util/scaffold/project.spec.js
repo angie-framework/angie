251fdfed84341f321becf8d4074723619e241d64
benderTheCrime <joseph.groseclose@spongecell.com>
2015-08-24 21:24:02 -0400

    Removed my home directory from tests because thats really stupid...

--- test/src/util/scaffold/project.spec.js
+++ test/src/util/scaffold/project.spec.js
@@ -163,35 +163,37 @@ describe('$$createProject', function() {
         expect(p.exit.calls[0].args[0]).to.eq(0);
     });
     it('test successful project creation with no location', function() {
+        const CWD = p.cwd();
+
         project({
             name: 'test'
         });
-        expect(fs.mkdirSync.calls[0].args[0]).to.eq('/Users/jg/angie/test');
-        expect(fs.mkdirSync.calls[1].args[0]).to.eq('/Users/jg/angie/test/src');
+        expect(fs.mkdirSync.calls[0].args[0]).to.eq(`${CWD}/test`);
+        expect(fs.mkdirSync.calls[1].args[0]).to.eq(`${CWD}/test/src`);
         expect(fs.mkdirSync.calls[2].args[0]).to.eq(
-            '/Users/jg/angie/test/src/constants'
+            `${CWD}/test/src/constants`
         );
         expect(fs.mkdirSync.calls[3].args[0]).to.eq(
-            '/Users/jg/angie/test/src/configs'
+            `${CWD}/test/src/configs`
         );
         expect(fs.mkdirSync.calls[4].args[0]).to.eq(
-            '/Users/jg/angie/test/src/services'
+            `${CWD}/test/src/services`
         );
         expect(fs.mkdirSync.calls[5].args[0]).to.eq(
-            '/Users/jg/angie/test/src/controllers'
+            `${CWD}/test/src/controllers`
         );
         expect(fs.mkdirSync.calls[6].args[0]).to.eq(
-            '/Users/jg/angie/test/src/directives'
+            `${CWD}/test/src/directives`
         );
         expect(
             fs.mkdirSync.calls[7].args[0]
-        ).to.eq('/Users/jg/angie/test/test');
+        ).to.eq(`${CWD}/test/test`);
         expect(
             fs.mkdirSync.calls[8].args[0]
-        ).to.eq('/Users/jg/angie/test/static');
+        ).to.eq(`${CWD}/test/static`);
         expect(
             fs.mkdirSync.calls[9].args[0]
-        ).to.eq('/Users/jg/angie/test/templates');
+        ).to.eq(`${CWD}/test/templates`);
         expect(promptly.confirm.calls[0].args[0]).to.eq(
             `${bold(green('Do you want Angie to cache static assets?'))} :`
         );
@@ -206,7 +208,7 @@ describe('$$createProject', function() {
         ]);
         expect(util.format.calls[0].args[4].val).to.eq(true);
         expect(fs.writeFileSync.calls[0].args).to.deep.eq([
-            '/Users/jg/angie/test/AngieFile.json', 'test', 'utf8'
+            `${CWD}/test/AngieFile.json`, 'test', 'utf8'
         ]);
         expect(
             $LogProvider.info.calls[0].args[0]
